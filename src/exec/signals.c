33f31b29c9c27b4837963e3297457a15e36faa33
manuelcuesta2005 <macuba2005@gmail.com>
2025-06-11 14:18:09 +0200

    created signals of minishell

---
+++ src/exec/signals.c
@@ -0,0 +1,44 @@
+#include "minishell.h"
+
+int     g_context;
+
+static void     aux_signals(int signal)
+{
+    if (g_context == 3)
+    {
+        write(STDOUT_FILENO, "\n", 1);
+        g_context = 4;
+    }
+    else if (g_context == 0 || g_context == 2)
+        g_context = signal;
+}
+static void    handle_signals(int signal)
+{
+    (void)signal;
+    if (g_context == 0)
+    {
+        write(STDOUT_FILENO, "\n", 1);
+        rl_replace_line("", 0);
+        rl_on_new_line();
+        rl_redisplay();
+    }
+    else if (g_context == 1)
+    {
+        write(STDOUT_FILENO, "\n", 1);
+        g_context = 0;
+    }
+    else if (g_context == 2)
+    {
+        write(STDOUT_FILENO, "\n", 1);
+        ioctl(STDIN_FILENO, TIOCSTI, (char *)"\n");
+        exit(1);
+    }
+    aux_signals(signal);
+}
+
+void    init_signals(void)
+{
+    g_context = 0;
+    signal(SIGINT, handle_signals);
+    signal(SIGQUIT, SIG_IGN);
+}
